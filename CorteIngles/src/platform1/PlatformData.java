cc7b060b12ba3f1ba3b3b4d70490f23b4b3875ab
Samuel Gómez Sánchez <samuelg@usal.es>
2018-10-19 13:31:48 +0200

    19/10 Interfaces.txt defined.

--- CorteIngles/src/platform1/PlatformData.java
+++ CorteIngles/src/platform1/PlatformData.java
@@ -9,16 +9,31 @@ package platform1;
  */
 public class PlatformData {
 
-	// Agents' alias
+	// Agents' aliases
 	public static final String USER_ALIAS = "user-agent";
 	public static final String RESERVATION_ALIAS = "reservation-agent";
 	public static final String ACTIVITY_ALIAS = "activity-agent";
 	public static final String CORTE_INGLES_ALIAS = "corte-ingles-agent";
 	
 	// Services' names and types
+	/**
+	 * Service provided by ActivityAgent
+	 */
 	public static final String RETRIEVE_ACTIVITY_SER = "retrieve-activity";
+	
+	/**
+	 * Service provided by ReservationAgent
+	 */
 	public static final String MAKE_RESERVATION_SER = "make-reservation";
+	
+	/**
+	 * Services provided by CorteInglesAgent
+	 */
 	public static final String HANDLE_RESERVATION_SER = "handle-reservation-request";
 	public static final String HANDLE_ACTIVITY_SER = "handle-activity-request";
+	
+	/**
+	 * Service provided by UserAgent
+	 */
 	public static final String HANDLE_USER_REQUEST_SER = "handle-user-request";
 }
